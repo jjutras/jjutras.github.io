94f14673de92eb4bb80984adc0ac3b55f8e408fb
jjutras <2436742@carrefour.cegepvicto.ca>
2024-12-10 12:57:21 -0500

    question

--- quiz/javaquiz.js
+++ quiz/javaquiz.js
@@ -9,10 +9,14 @@ class Question {
     }
   }
   let questions = [
-    new Question("", ["indexOf()", "map()", "filter()", "reduce()"], "filter()"),
-    new Question("Quelle méthode Javascript permet de vérifier si un élément figure dans un tableau", ["isNaN()","includes()", "findIndex()", "isOdd()"], "includes()"),
-    new Question("Quelle méthode transforme du JSON en un objet Javascript ?", ["JSON.parse()","JSON.stringify()", "JSON.object()", "JSON.toJS"], "JSON.parse()"),
-    new Question("Quel objet Javascript permet d'arrondir à l'entier le plus proche", ["Math.ceil()","Math.floor()", "Math.round()", "Math.random()"], "Math.round()")
+    new Question("Qui est le plus grand marqueur de tout les temps dans la LNH en carrière", ["Mario Lemieux", "Steve Shutt", "Wayne Gretzky", "Alfie Turcotte"], "Wayne Gretzky"),
+    new Question("Quelle est l'équipe ayant gagné le plus grand nombre de coupe Stanley", ["Canadiens de Montreal","Huricannes de la Caroline", "Rangers de New York", "Whalers de Hartford"], "Canadiens de Montreal"),
+    new Question("Qui est le Quebecois ayant marqué 50 buts en 50 parties", ["Mario Lemieux","Mike Bossy", "Gilbert Perrault", "Guy Lafleur"], "Mike Bossy"),
+    new Question("Quelle défenseur de la NHL qui a joué 26 saisons", ["Raymond Bourque","Tim Hortons", "Chris Cherlios", "Doug Harvey"], "Chris Cherlios"),
+    new Question("Qui détient le record du plus grand nombre de minutes de punition en carrière", ["Dale Hunter", "Dave Williams", "Tie Domi", "Rob Ray"], "Dave Williams"),
+    new Question("Qui a marqué le plus de point lors de sa saison recrue", ["Teemu Selanne","Peter Stastny", "Alex Ovechkin", "Dale Hawerchuk"], "Teemu Selanne"),
+    new Question("Qui est le defenseur avec le plus grand nombre de but en carrère", ["Phil Housley","Al MacInnis", "Paul Coffey", "Raymond Bourque"], "Raymond Bourque"),
+    new Question("Qui est le joueurs avec le plus grand nombre de partie jouer consécutive en carrière", ["Doug Jarvis","Keith Yandle", "Phil Kessel", "Garry Unger"], "Phil Kessel")
   ];
   
   class Quiz {
@@ -45,6 +49,11 @@ class Question {
         <h1>Quiz terminé !</h1>
         <h3> Votre score est de : ${quiz.score} / ${quiz.questions.length}</h3>`;
       this.elementShown("quiz", endQuizHTML);
+      
+      const messages = messageQuiz();
+      const messagesss = document.getElementById("messagesss");
+      messagesss.textContent = `${messages}`;
+
     },
     question: function() {
       this.elementShown("question", quiz.getCurrentQuestion().text);
@@ -83,3 +92,14 @@ class Question {
   // Create Quiz
   let quiz = new Quiz(questions);
   quizApp();
+
+  function messageQuiz(){
+
+  
+    if(quiz.score = quiz.questions,length){
+      return "Félicitation vous connaîssez bien les records"
+   }
+    else{
+      return "Vous pouvez recommencer"
+    }
+  }
